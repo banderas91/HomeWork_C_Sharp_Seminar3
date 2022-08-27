dcf1747edf320539e9caaca49281314918c8e8a8
K_A_A <banderas91@mail.ru>
2022-08-27 15:28:46 +0300

    Задача_3_Доработанная

--- zadacha_3/Program.cs
+++ zadacha_3/Program.cs
@@ -1,17 +1,23 @@
 ﻿//Напишите программу, которая принимает на вход число (N) и помещает в массив таблицу кубов чисел от 1 до N. Полученный массив вывести на экран.
 
 
-Console.Write("Введите число   ");
+Console.Write("ведите число   ");
 int N =Convert.ToInt32(Console.ReadLine());
+if (N == 0)
+{
+    Console.WriteLine("Введите число отличное от 0");
+}
 
 int[] array = new int [N];
 
-int index = 0;
-int num = 1;
-while(index < N)
-{
-    
-    Console.WriteLine((Math.Pow(num, 3)));
+ int index = 0;
+ int num = 1;
+
+ while(index < N)
+ {
+    Console.Write("Куб числа "+num+"----> " );
+    array[index] = Convert.ToInt32(Math.Pow(num, 3));
+    Console.WriteLine(array[index]);
     num++;
     index++;
 }
